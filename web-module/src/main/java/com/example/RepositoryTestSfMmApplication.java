98dc3a134c437a1a0e4155df49489791edb21478
Junior Java Developer Mentor <kontakt@juniorjavadeveloper.pl>
2021-03-16 11:41:58 +0100

    Add Spring initial files with Maven

---
+++ web-module/src/main/java/com/example/RepositoryTestSfMmApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryTestSfMmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryTestSfMmApplication.class, args);
+	}
+
+}
